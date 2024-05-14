a5ceeb2f68340e6bab2f8fd1118d1b0f7bf77fe2
brlim <>
2024-05-13 23:57:49 -0400

    add color column and remove transparent background

--- nvim/lua/plugins/catppuccin_nvim.lua
+++ nvim/lua/plugins/catppuccin_nvim.lua
@@ -2,9 +2,6 @@ return {
   'catppuccin/nvim',
   name = 'catppuccin',
   priority = 1000,
-  opts = {
-    transparent_background = true,
-  },
   init = function()
     vim.cmd.colorscheme 'catppuccin'
   end,
