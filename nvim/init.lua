72ac8b01d622ff4039102b2e97000e692b3ba9d5
brlim <>
2024-05-17 21:09:37 -0400

    chore: clean up config file

--- nvim/init.lua
+++ nvim/init.lua
@@ -5,16 +5,13 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = true
+vim.g.have_nerd_font = false
 
 -- [[ Setting options ]]
 -- See `:help vim.opt`
 
--- Make line numbers default
+-- Set line numbers.
 vim.opt.number = true
--- You can also add relative line numbers, to help with jumping.
---  Experiment for yourself to see if you like it!
--- vim.opt.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a'
