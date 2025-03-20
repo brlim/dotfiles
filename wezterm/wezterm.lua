716df9a66d9d095de81a0b28ffa62b404491e0c9
brlim <>
2025-03-20 18:48:44 -0400

    change font size and add custom key mappings to select different panels

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,7 +1,37 @@
 local wezterm = require("wezterm")
 
+local act = wezterm.action
+
 return {
 	window_background_opacity = 0.87,
-	font_size = 14,
+	font_size = 16,
 	font = wezterm.font("Berkeley Mono"),
+
+	keys = {
+		{
+			key = "h",
+			mods = "CMD|SHIFT",
+			action = act.ActivatePaneDirection("Left"),
+		},
+		{
+			key = "l",
+			mods = "CMD|SHIFT",
+			action = act.ActivatePaneDirection("Right"),
+		},
+		{
+			key = "k",
+			mods = "CMD|SHIFT",
+			action = act.ActivatePaneDirection("Up"),
+		},
+		{
+			key = "j",
+			mods = "CMD|SHIFT",
+			action = act.ActivatePaneDirection("Down"),
+		},
+		{
+			key = "w",
+			mods = "CMD|SHIFT",
+			action = wezterm.action.CloseCurrentPane({ confirm = true }),
+		},
+	},
 }
