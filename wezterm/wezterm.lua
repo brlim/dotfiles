a524c24d93b77a0c68d866beb7ee92a067de30b7
brlim <>
2025-04-20 11:21:06 -0400

    feat: add font fallback and refactor config file

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,37 +1,41 @@
 local wezterm = require("wezterm")
+local config = wezterm.config_builder()
+
+config.window_background_opacity = 0.87
+config.font_size = 16
+config.font = wezterm.font_with_fallback({
+	"Berkeley Mono",
+	"JetBrains Mono",
+})
 
 local act = wezterm.action
 
-return {
-	window_background_opacity = 0.87,
-	font_size = 16,
-	font = wezterm.font("Berkeley Mono"),
-
-	keys = {
-		{
-			key = "h",
-			mods = "CMD|SHIFT",
-			action = act.ActivatePaneDirection("Left"),
-		},
-		{
-			key = "l",
-			mods = "CMD|SHIFT",
-			action = act.ActivatePaneDirection("Right"),
-		},
-		{
-			key = "k",
-			mods = "CMD|SHIFT",
-			action = act.ActivatePaneDirection("Up"),
-		},
-		{
-			key = "j",
-			mods = "CMD|SHIFT",
-			action = act.ActivatePaneDirection("Down"),
-		},
-		{
-			key = "w",
-			mods = "CMD|SHIFT",
-			action = wezterm.action.CloseCurrentPane({ confirm = true }),
-		},
+config.keys = {
+	{
+		key = "h",
+		mods = "CMD|SHIFT",
+		action = act.ActivatePaneDirection("Left"),
+	},
+	{
+		key = "l",
+		mods = "CMD|SHIFT",
+		action = act.ActivatePaneDirection("Right"),
+	},
+	{
+		key = "k",
+		mods = "CMD|SHIFT",
+		action = act.ActivatePaneDirection("Up"),
+	},
+	{
+		key = "j",
+		mods = "CMD|SHIFT",
+		action = act.ActivatePaneDirection("Down"),
+	},
+	{
+		key = "w",
+		mods = "CMD|SHIFT",
+		action = wezterm.action.CloseCurrentPane({ confirm = true }),
 	},
 }
+
+return config
