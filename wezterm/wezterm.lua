6298db660da184225510f787d877c9097a03a6f7
brlim <>
2025-04-21 11:28:57 -0400

    feat: add OS-specific code to config file and delete Windows OS config file

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -2,7 +2,6 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 config.window_background_opacity = 0.87
-config.font_size = 16
 config.font = wezterm.font_with_fallback({
 	"Berkeley Mono",
 	"JetBrains Mono",
@@ -10,32 +9,64 @@ config.font = wezterm.font_with_fallback({
 
 local act = wezterm.action
 
-config.keys = {
-	{
-		key = "h",
-		mods = "CMD|SHIFT",
-		action = act.ActivatePaneDirection("Left"),
-	},
-	{
-		key = "l",
-		mods = "CMD|SHIFT",
-		action = act.ActivatePaneDirection("Right"),
-	},
-	{
-		key = "k",
-		mods = "CMD|SHIFT",
-		action = act.ActivatePaneDirection("Up"),
-	},
-	{
-		key = "j",
-		mods = "CMD|SHIFT",
-		action = act.ActivatePaneDirection("Down"),
-	},
-	{
-		key = "w",
-		mods = "CMD|SHIFT",
-		action = wezterm.action.CloseCurrentPane({ confirm = true }),
-	},
-}
+if wezterm.target_triple == "x86_64-apple-darwin" or wezterm.target_triple == "aarch64-apple-darwin" then
+	config.font_size = 16
+	config.keys = {
+		{
+			key = "h",
+			mods = "CMD|SHIFT",
+			action = act.ActivatePaneDirection("Left"),
+		},
+		{
+			key = "l",
+			mods = "CMD|SHIFT",
+			action = act.ActivatePaneDirection("Right"),
+		},
+		{
+			key = "k",
+			mods = "CMD|SHIFT",
+			action = act.ActivatePaneDirection("Up"),
+		},
+		{
+			key = "j",
+			mods = "CMD|SHIFT",
+			action = act.ActivatePaneDirection("Down"),
+		},
+		{
+			key = "w",
+			mods = "CMD|SHIFT",
+			action = wezterm.action.CloseCurrentPane({ confirm = true }),
+		},
+	}
+elseif wezterm.target_triple == "x86_64-pc-windows-msvc" then
+	config.font_size = 12
+	config.keys = {
+		{
+			key = "h",
+			mods = "ALT",
+			action = act.ActivatePaneDirection("Left"),
+		},
+		{
+			key = "l",
+			mods = "ALT",
+			action = act.ActivatePaneDirection("Right"),
+		},
+		{
+			key = "k",
+			mods = "ALT",
+			action = act.ActivatePaneDirection("Up"),
+		},
+		{
+			key = "j",
+			mods = "ALT",
+			action = act.ActivatePaneDirection("Down"),
+		},
+		{
+			key = "w",
+			mods = "ALT|CMD|SHIFT",
+			action = wezterm.action.CloseCurrentPane({ confirm = true }),
+		},
+	}
+end
 
 return config
